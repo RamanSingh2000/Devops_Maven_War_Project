8f0156e39ee1cfa20628613062dbc04e41dad062
RamanSingh2000 <145480618+RamanSingh2000@users.noreply.github.com>
2023-11-01 15:24:01 +0530

    Update DevopsMavenProject13Application.java

--- src/main/java/com/example/demo/DevopsMavenProject13Application.java
+++ src/main/java/com/example/demo/DevopsMavenProject13Application.java
@@ -12,7 +12,7 @@ public class DevopsMavenProject13Application {
 
 	@GetMapping
 	public String message(){
-		return "Hello Dev Team";
+		return "Hello Dev Team.Hurray!We have deployed the latest build";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsMavenProject13Application.class, args);
