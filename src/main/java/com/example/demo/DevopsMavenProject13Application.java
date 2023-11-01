1eb73fe86258d2a1048c23730c6f1ad2db34ad0f
RamanSingh2000 <145480618+RamanSingh2000@users.noreply.github.com>
2023-11-01 11:45:15 +0530

    Update DevopsMavenProject13Application.java

--- src/main/java/com/example/demo/DevopsMavenProject13Application.java
+++ src/main/java/com/example/demo/DevopsMavenProject13Application.java
@@ -12,7 +12,7 @@ public class DevopsMavenProject13Application {
 
 	@GetMapping
 	public String message(){
-		return "Hello Dev Team,Deployment has been Successful.Website is running Successfully";
+		return "Hello Dev Team,Deployment has been Successful.Website is running Successfully.Please look into it";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsMavenProject13Application.class, args);
